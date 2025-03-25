0463938af49adfd8c7fe7685b89876a46af6b36f
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-03-25 23:14:19 +0300

    Completed code-along

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,30 @@
 class MySet:
+    # initialize a new MySet and add values
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    # remove a value from the set and return the updated set
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
